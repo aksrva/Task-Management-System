1723748666039d5ba1bbcfaa893303e1f1edfeb8
aksrva <akp31378@gmail.com>
2023-09-03 10:40:12 +0530

    Task Management System Complete Drag and Drop

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const absPath = path.join(__dirname, "public");
+const ejs = require("ejs");
+app.use(express.static(absPath));
+app.use(express.json());
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
+// app engine
+app.set("view engine", "ejs");
+app.set("views", absPath + "/views");
+// connect database
+const url = "mongodb://127.0.0.1:27017/kanban-task-management";
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed -> " + err.message);
+  });
+
+const taskSchema = new mongoose.Schema({
+  task: { type: String, required: true },
+  task_description: { type: String, required: true },
+  status: { type: String, required: true },
+});
+const Task = mongoose.model("Task", taskSchema, "tasks");
+
+app.get("/", async (req, res) => {
+  const todo = await Task.find({ status: "todo" }, "-status");
+  const doing = await Task.find({ status: "doing" }, "-status");
+  const done = await Task.find({ status: "done" }, "-status");
+  res.render("index", { todo, doing, done });
+});
+app.post("/", async (req, res) => {
+  try {
+    const newTask = new Task(req.body);
+    const resp = await newTask.save();
+    if (!resp) {
+      throw new Error("Card not saved!");
+    }
+    res.status(200).json(resp);
+  } catch (err) {
+    res.status(400).json("Error" + err.message);
+  }
+});
+app.put("/update/:update_id/:uTitle/:uDesc", async (req, res) => {
+  try {
+    const uId = req.params.update_id;
+    const updateResp = await Task.updateOne(
+      { _id: uId },
+      { $set: { task: req.params.uTitle, task_description: req.params.uDesc } }
+    );
+    if (updateResp.modifiedCount === 1) {
+      res.status(200).json("Task Updated");
+    }
+  } catch (err) {
+    res.status(400).json("Task Updated Failed!!");
+  }
+});
+app.delete("/:task_list/:del_id", async (req, res) => {
+  const taskList = req.params.task_list;
+  const delId = req.params.del_id;
+  try {
+    const delResp = await Task.deleteOne({ _id: delId });
+    if (delResp.deletedCount == 0) {
+      throw new Error("Task Not deleted");
+    }
+    const taskData = await Task.find({ status: taskList });
+    res.status(200).json(taskData);
+  } catch (err) {
+    res.status(400).json("Task Deleted Failed!!");
+  }
+});
+app.put("/:uStatus/:update_id", async (req, res) => {
+  try {
+    const uStatus = req.params.uStatus;
+    const uId = req.params.update_id;
+
+    const updateResp = await Task.updateOne(
+      { _id: uId },
+      { $set: { status: uStatus } }
+    );
+    if (updateResp.modifiedCount === 1) {
+      res.status(200).json("Task Updated");
+    }
+  } catch (err) {
+    res.status(400).json("Task Updated Failed!!");
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log("Server is started at port " + PORT);
+});
---
+++ public/js/main.js
@@ -0,0 +1,155 @@
+const status = ["todo", "doing", "done"];
+const addBtn = document.querySelectorAll(".new-task");
+const addForm = document.querySelectorAll(".add-task");
+const editForm = document.querySelectorAll(".edit-task");
+const taskForm = document.querySelectorAll("textarea[name='new-form'");
+const taskTitle = document.querySelectorAll("input[name='title']");
+const tasks = document.querySelectorAll(".tasks");
+addBtn.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    addForm.forEach((form) => {
+      form.style.display = "none";
+    });
+    addForm[idx].style.display = "block";
+  });
+});
+const closeForm = document.querySelectorAll(".close-form");
+closeForm.forEach((closebtn, idx) => {
+  closebtn.addEventListener("click", () => {
+    addForm[idx].style.display = "none";
+  });
+});
+const closeEditForm = document.querySelectorAll(".edit-close-form");
+closeEditForm.forEach((closebtn, idx) => {
+  closebtn.addEventListener("click", () => {
+    editForm[idx].style.display = "none";
+  });
+});
+
+const submitForm = document.querySelectorAll(".submit-form");
+submitForm.forEach((sbmtBtn, idx) => {
+  sbmtBtn.addEventListener("click", () => {
+    console.log(taskForm[idx].value)
+    if (taskForm[idx].value === "") return;
+    const request = new XMLHttpRequest();
+    request.open("POST", "/", true);
+    request.setRequestHeader(
+      "Content-type",
+      "application/x-www-form-urlencoded"
+    );
+    request.send(
+      `task=${taskTitle[idx].value}&task_description=${taskForm[idx].value}&status=${status[idx]}`
+    );
+    request.addEventListener("readystatechange", () => {
+      if (request.status == 200 && request.readyState == 4) {
+        const aTask = JSON.parse(request.response);
+        updateUI([aTask], idx);
+      } else if (request.status == 400 && request.readyState == 4) {
+        alert("ERROR -> " + request.response);
+      }
+    });
+    taskForm[idx].value = "";
+  });
+});
+
+// Delete
+const delTask = document.querySelectorAll(".task-del");
+
+function deleteTask(delId, taskList) {
+  const request = new XMLHttpRequest();
+  request.open("DELETE", `${status[Number(taskList)]}/${delId}`, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send();
+  request.addEventListener("readystatechange", () => {
+    if (request.status == 200 && request.readyState == 4) {
+      const aTask = JSON.parse(request.response);
+      updateUI(aTask, taskList);
+    } else if (request.status == 400 && request.readyState == 4) {
+      alert("ERROR -> " + request.response);
+    }
+  });
+}
+
+const updateUI = (utasks, tasklist) => {
+  // utasks.forEach((task) => {
+  //   tasks[
+  //     tasklist
+  //   ].innerHTML += `<div class="task" data-id="${task._id}" draggable="true">
+  //   <p>${task.task}</p>
+  //   <a
+  //   class="task-del"
+  //   href="javascript:void(0)"
+  //   onclick="deleteTask('${task._id}', '${tasklist}')"
+  //   >❌</a>
+  //   <a href="javascript:void(0)" data-id="<%= task._id  %>">📝</a>
+  //   </div>`;
+  // });
+  window.location.href = "/";
+};
+
+// draggable
+const taskList = document.querySelectorAll(".task");
+const todo = document.getElementById("todo");
+const doing = document.getElementById("doing");
+const done = document.getElementById("done");
+
+taskList.forEach((task) => {
+  task.addEventListener("dragstart", (e) => {
+    let selected = e.target;
+    doing.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    doing.addEventListener("drop", (e) => {
+      updatedTaskStatus(doing, selected);
+      selected = null;
+    });
+    done.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    done.addEventListener("drop", (e) => {
+      updatedTaskStatus(done, selected);
+      selected = null;
+    });
+    todo.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    todo.addEventListener("drop", (e) => {
+      updatedTaskStatus(todo, selected);
+      selected = null;
+    });
+  });
+});
+
+const updatedTaskStatus = (tasklist, selectedTask) => {
+  const uStatus = tasklist.getAttribute("id");
+  const taskId = selectedTask.getAttribute("data-id");
+
+  const request = new XMLHttpRequest();
+  request.open("PUT", `${uStatus}/${taskId}`, true);
+
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send();
+  tasklist.appendChild(selectedTask);
+};
+
+// update task
+const updateTask = (taskId, tasktitle, taskDesc, idx) => {
+  const editForm = document.querySelectorAll(".edit-task");
+  const editTitle = document.querySelectorAll("input[name='edtitle']");
+  const editDesc = document.querySelectorAll(".editDesc");
+  editForm.forEach((form) => {
+    form.style.display = "none";
+  });
+  editForm[idx].style.display = "block";
+  editTitle[idx].value = tasktitle;
+  editDesc[idx].value = taskDesc;
+
+  const editBtn = document.querySelectorAll(".edit-form")[idx];
+  editBtn.addEventListener("click", () => {
+    const request = new XMLHttpRequest();
+    request.open("PUT", `update/${taskId}/${editTitle[idx].value}/${editDesc[idx].value}`, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send();
+    window.location.href = "/"
+  });
+};
